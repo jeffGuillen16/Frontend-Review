de69e37837554cd66472f4899078ceabb2fbf48e
jeffGuillen16 <marcoguillen97@outlook.com>
2022-06-30 18:16:15 -0300

    cambios

--- javacript/19CallBack/app.js
+++ javacript/19CallBack/app.js
@@ -1,2 +1,14 @@
  //callback
 
+function modify(array, callback){
+  //hacemos algo...
+  array.push('midu')
+  //despues de hacer algo..
+setTimeout(function(){ callback(array)},3000)
+}
+
+const names=['gartiel','vedaskies','tomimelo']
+
+modify(names,function(array){
+  console.log(`he modificado el array y ahora es de ${ array.length } elementos!`)
+})
