5c38f8c62f3715d3bf81c2497b0a943402217c49
jeffGuillen16 <marcoguillen97@outlook.com>
2022-08-20 17:51:19 -0300

    cambios

--- javacript/26.DOM/app.js
+++ javacript/26.DOM/app.js
@@ -3,7 +3,7 @@
     el mismo body o incluso las etiquetas de una lista . Con javascript podemos
     modificar estas etiquetas con el uso de eventos.
 */
-
+/* ############################Selectore########################## */
 console.log(document.getElementsByTagName('li'));
 console.log(document.getElementsByClassName('card'));
 console.log(document.getElementsByName("usuario"))
@@ -16,6 +16,7 @@ console.log(document.querySelector("li"));
 /* querySelectorAll("")*/
 console.log(document.querySelectorAll("li"));
 
+/* #################Atributos#################################### */
 document.documentElement.lang="en";
 console.log(document.documentElement.lang);
 document.documentElement.setAttribute("lang","es-MX");
@@ -25,3 +26,15 @@ console.log(document.documentElement.lang);
     con "$") */
 const $linkDOM=document.querySelector(".link-dom");
 $linkDOM.setAttribute("target","_blank");
+$linkDOM.setAttribute("rel","noopener");
+$linkDOM.setAttribute("href","https://www.youtube.com/watch?v=Wv2rLZmbPMA&list=RDWv2rLZmbPMA&start_radio=1");
+console.log($linkDOM.hasAttribute("rel"));//Tiene rel
+$linkDOM.removeAttribute("rel");
+console.log($linkDOM.hasAttribute("rel"));
+
+/* ##################### Data-Attributes##########################*/
+console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description","Modelo de Objeto del Documento");
+console.log($linkDOM.dataset.description);
---
+++ javacript/block/app.js
@@ -0,0 +1,11 @@
+/* ----Prueba 1 Arrow function en diferencia con la funcion tipica -> Respeta el orden sintactico*/
+const saludar=()=>console.log("holaaaaa");
+saludar();
+
+
+/* ---Prueba 2---- 
+function saludar(){
+    console.log("holaaaa");
+};
+saludar(); */
+
