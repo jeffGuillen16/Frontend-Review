94eab9f7f7d25768bdc4d83fe94bb7e43d365413
jeffGuillen16 <marcoguillen97@outlook.com>
2023-11-11 12:22:05 -0300

    Enviar datos

---
+++ auto_commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Verifica si se proporcionó un comentario para el commit
+if [ -z "$1" ]; then
+    echo "Por favor, proporciona un comentario para el commit."
+    exit 1
+fi
+
+# Cambia a la ruta de tu repositorio
+cd C:\Users\marco\OneDrive\Escritorio\code\Frontend-Review
+
+# Añade todos los cambios
+git add .
+
+# Realiza el commit con el comentario proporcionado
+git commit -m "$1"
+
+# Realiza el push al repositorio remoto
+git push origin nombre-de-tu-rama
